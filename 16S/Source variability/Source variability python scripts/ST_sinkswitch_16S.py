a0883ff96181b00b7914d657bca3908e71f6ccc3
Timothy Lim <timothy.lim@monash.edu>
2025-04-14 12:13:51 +1000

    Add files via upload
    Added "&" at the end of each SourceTracker line to process all in one go.

--- 16S/Source variability/Source variability python scripts/ST_sinkswitch_16S.py
+++ 16S/Source variability/Source variability python scripts/ST_sinkswitch_16S.py
@@ -17,7 +17,7 @@ namefile= [os.path.splitext(filename)[0] for filename in os.listdir("S:\Source_v
 #use nested for loop, first run 5 times, then switch to another metadata file for the entire range of the loop
 for x in range(len(files)):
     for runs in range(1,6):    
-        script = 'sourcetracker2 gibbs -i /fs03/hj18/Tim_duplicate/EPA/Phylo/core-metrics-results/feature-table.tsv -m /fs03/hj18/Tim_duplicate/EPA/Source_variability/Diversity/Modified_metadata/%s -o /fs03/hj18/Tim_duplicate/EPA/Source_variability/SourceTracker/Exports/%s_r%s --burnin 100 --source_rarefaction_depth 10000 --sink_rarefaction_depth 10000 \n' %(files[x],namefile[x],runs)
+        script = 'sourcetracker2 gibbs -i /fs03/hj18/Tim_duplicate/EPA/Phylo/core-metrics-results/feature-table.tsv -m /fs03/hj18/Tim_duplicate/EPA/Source_variability/Diversity/Modified_metadata/%s -o /fs03/hj18/Tim_duplicate/EPA/Source_variability/SourceTracker/Exports/%s_r%s --burnin 100 --source_rarefaction_depth 10000 --sink_rarefaction_depth 10000 & \n' %(files[x],namefile[x],runs)
         with open("ST_sinkswitch_16S.txt",'a') as f:
                 f.write (script)
         
