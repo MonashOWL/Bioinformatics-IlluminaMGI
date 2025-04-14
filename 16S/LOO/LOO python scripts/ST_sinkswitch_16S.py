a745947035438bc21da61759fb26370d22dfa394
Timothy Lim <timothy.lim@monash.edu>
2025-04-14 11:44:53 +1000

    Add files via upload

--- 16S/LOO/LOO python scripts/ST_sinkswitch_16S.py
+++ 16S/LOO/LOO python scripts/ST_sinkswitch_16S.py
@@ -17,7 +17,7 @@ namefile= [os.path.splitext(filename)[0] for filename in os.listdir("G:\My Drive
 #use nested for loop, first run 5 times, then switch to another metadata file for the entire range of the loop
 for x in range(len(files)):
     for runs in range(1,6):    
-        script = 'sourcetracker2 gibbs -i /fs04/hj18/Timothy/Tarago_24022022_Wastewater/Merged_with_24022022_Combined/Phylo/Without_excluding_septics/core-metrics-results/feature-table.tsv -m /fs04/hj18/Timothy/Tarago_24022022_Wastewater/Merged_with_24022022_Combined/OneAtATime/Diversity/%s -o /fs04/hj18/Timothy/Tarago_24022022_Wastewater/Merged_with_24022022_Combined/OneAtATime/SourceTracker/Exports/%s_r%s --burnin 100 --source_rarefaction_depth 2000 --sink_rarefaction_depth 2000 \n' %(files[x],namefile[x],runs)
+        script = 'sourcetracker2 gibbs -i /fs04/hj18/Timothy/Tarago_24022022_Wastewater/Merged_with_24022022_Combined/Phylo/Without_excluding_septics/core-metrics-results/feature-table.tsv -m /fs04/hj18/Timothy/Tarago_24022022_Wastewater/Merged_with_24022022_Combined/OneAtATime/Diversity/%s -o /fs04/hj18/Timothy/Tarago_24022022_Wastewater/Merged_with_24022022_Combined/OneAtATime/SourceTracker/Exports/%s_r%s --burnin 100 --source_rarefaction_depth 2000 --sink_rarefaction_depth 2000 & \n' %(files[x],namefile[x],runs)
         with open("ST_sinkswitch_16S.txt",'a') as f:
                 f.write (script)
         
